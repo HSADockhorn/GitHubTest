bd4446b022cb06a4f3180a2fd969f7fc8dcc7fd9
HSADockhorn <hsadockhorn@icloud.com>
2022-11-11 14:47:02 +0100

    V3. Reformulated the mathematics.

--- GitHubTest/main.c
+++ GitHubTest/main.c
@@ -2,13 +2,14 @@
 //  main.c
 //  GitHubTest
 //
-//	 The exsample used derives from Project Euler problem 3 which amounts to determining
+//	 The example derives from Project Euler problem 3 which amounts to determining
 //  the largest prime factor of the number 600851475143.
 //
 //  Created by Hans Dockhorn on 11/11/2022.
 //
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, const char * argv[])
 {
@@ -16,16 +17,24 @@ int main(int argc, const char * argv[])
 	unsigned long N = 600851475143;
 	unsigned long p = 3;
 	
+	// Register starting time
+	clock_t startTime = clock();
+	
 	// Do the math
 	while ( p*p <= N )
 	{
-		while ( N%p == 0 ) N /= p;
-		p += 2;
+		if ( N%p == 0 ) N /= p; else p += 2;
 	}
 
+	// Register ending time
+	clock_t endTime = clock();
+	
 	// Display result
 	printf( "The largest prime factor of 600851475143 is %ld.\n", N );
 
-	// Wrap-up
+	// Display computation time in clock ticks
+	printf( "The computation took %ld clock ticks.\n", endTime-startTime );
+	
+	// Wrap up
 	return 0;
 }
